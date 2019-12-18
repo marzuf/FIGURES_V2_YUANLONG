f1df83d257478fc70bf813682fe94d70297215f3
mzuer <z_marie_7@hotmail.com>
2019-12-18 18:58:32 +0100

    18b.12.19

--- FCC_fract_pie_ds.R
+++ FCC_fract_pie_ds.R
@@ -1,12 +1,93 @@
+hicds <- "LG1_40kb"
+exprds <- "TCGAluad_norm_luad"
+
+plotType <- "png"
+
+source("settings.R")
+require(ggplot2)
+require(ggsci)
+
+source("../Cancer_HiC_data_TAD_DA/utils_fct.R")
+
+# Rscript FCC_fract_pie_ds.R
+
+outFolder <- file.path("FCC_FRACT_PIE_DS")
+dir.create(outFolder, recursive = TRUE)
+
+inDT <- get(load("BARPLOT_WITH_FCC_FRACT/all_dt.Rdata"))
+ds_dt <- inDT[inDT$hicds == hicds & inDT$exprds == exprds,]
+stopifnot(sum(ds_dt$countFCC) == 1)
+ds_dt$pct <- ds_dt$countFCC * 100
+ds_dt$pct_lab <- paste0(round(ds_dt$pct, 2), " %")
+ds_dt$pct_lab_gg <- paste0(round(ds_dt$pct, 2), " %")
+ds_dt$pct_lab_gg[ds_dt$pct < 1] <- ""
+ds_dt$labs1 <- paste0(ds_dt$intervalFCC, "\n",ds_dt$pct_lab)
+ds_dt$labs <- paste0(ds_dt$intervalFCC, " (",ds_dt$pct_lab, ")")
+ds_dt$labs <- gsub("FCC", "", ds_dt$labs)
+ds_dt$labs <- factor(ds_dt$labs, levels = rev(as.character(ds_dt$labs)))
+
+ggsci_pal <- "lancet"
+ggsci_subpal <- ""
+mycols <- eval(parse(text = paste0("pal_", ggsci_pal, "(", ggsci_subpal, ")")))(length(ds_dt$labs))
+
+
+outFile <- file.path(outFolder, paste0(hicds, "_", exprds, "_FCCfract_pie_withLab.", plotType))
+do.call(plotType, list(outFile, height=myHeight*2, width=myWidth*2.5))
+pie(x=ds_dt$pct,labels = ds_dt$labs1, col=mycols,
+    cex=plotCex,
+    cex.main=2,
+    main=paste0("TAD FCC fract."))
+mtext(side=3, text = paste0(hicds, " - ", exprds), cex=plotCex)
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+ds_dt$labs_2 <- as.character(ds_dt$pct_lab)
+ds_dt$labs_2[ds_dt$pct < 1] <- ""
+
+outFile <- file.path(outFolder, paste0(hicds, "_", exprds, "_FCCfract_pie_withLeg.", plotType))
+do.call(plotType, list(outFile, height=myHeight*2, width=myWidth*2))
+pie(x=ds_dt$pct,labels = ds_dt$labs_2, col=mycols,
+    cex.main=2,
+    cex  =plotCex,
+    main=paste0("TAD FCC fract."))
+legend(
+  "bottomright",
+  legend = rev(ds_dt$labs),
+  pch=15,
+  col = mycols,
+  # horiz=TRUE
+  bty="n"
+)
+mtext(side=3, text = paste0(hicds, " - ", exprds), cex=plotCex)
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+
+
+pie_fract <- ggplot(ds_dt, aes(x="", y=pct, fill=labs))+ 
+  geom_bar(width = 1, stat = "identity")+
+  coord_polar("y", start=0) +
+  ggtitle("TAD FCC fract.", subtitle =  paste0(hicds, " - ", exprds))+
+  labs(fill="", color="")+
+  theme_minimal() +
+  theme(axis.text.x=element_blank(), axis.text.y=element_blank(),
+        axis.title.x=element_blank(), axis.title.y=element_blank(),
+        plot.title = element_text(hjust = 0.5, size=14, face="bold"),
+        plot.subtitle = element_text(hjust = 0.5, size=13),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank()
+        ) +
+  eval(parse(text=paste0("scale_color_", ggsci_pal, "(", ggsci_subpal, ")")))+
+  eval(parse(text=paste0("scale_fill_", ggsci_pal, "(", ggsci_subpal, ")"))) +
+geom_text(aes(y = pct/3 + c(0, cumsum(pct)[-length(pct)]), 
+              label = pct_lab_gg), size=5, color="black")
+
+outFile <- file.path(outFolder, paste0(hicds, "_", exprds, "_FCCfract_pie_ggplot.", plotType))
+ggsave(plot = pie_fract, filename = outFile, height=myHeightGG, width = myWidthGG)
+cat(paste0("... written: ", outFile, "\n"))
 
 
 
 
-
-slices <- c(10, 12, 4, 16, 8)
-lbls <- c("US", "UK", "Australia", "Germany", "France")
-pct <- round(slices/sum(slices)*100)
-lbls <- paste(lbls, pct) # add percents to labels
-lbls <- paste(lbls,"%",sep="") # ad % to labels
-pie(slices,labels = lbls, col=rainbow(length(lbls)),
-    main="Pie Chart of Countries") 
---
+++ nSignifGenes_nSignifTADs.R
@@ -0,0 +1,137 @@
+
+plotType <- "png"
+
+source("settings.R")
+require(ggsci)
+
+source("../Cancer_HiC_data_TAD_DA/utils_fct.R")
+
+source("../Yuanlong_Cancer_HiC_data_TAD_DA/subtype_cols.R")
+
+# Rscript nSignifGenes_nSignifTADs.R
+
+outFolder <- file.path("NSIGNIFGENES_NSIGNIFTADS")
+dir.create(outFolder, recursive = TRUE)
+
+geneSignifThresh <- 0.05
+tadSignifThresh <- 0.01
+
+gene_col <- pal_d3()(2)[1]
+tad_col <- pal_d3()(2)[2]
+
+
+inDT <- get(load("../v2_Yuanlong_Cancer_HiC_data_TAD_DA/GENE_RANK_TAD_RANK/all_gene_tad_signif_dt.Rdata"))
+geneDT <- inDT[,c("hicds", "exprds", "entrezID", "adj.P.Val")]
+geneDT <- unique(geneDT)
+nSignifGenes_dt <- aggregate(adj.P.Val~hicds + exprds, data = geneDT, FUN=function(x) sum(x<=geneSignifThresh))
+colnames(nSignifGenes_dt)[colnames(nSignifGenes_dt) == "adj.P.Val"] <- "nSignifGenes"
+
+tadDT <- inDT[,c("hicds", "exprds", "region", "tad_adjCombPval")]
+tadDT <- unique(tadDT)
+nSignifTADs_dt <- aggregate(tad_adjCombPval~hicds + exprds, data = tadDT, FUN=function(x) sum(x<=tadSignifThresh))
+colnames(nSignifTADs_dt)[colnames(nSignifTADs_dt) == "tad_adjCombPval"] <- "nSignifTADs"
+
+nSignif_dt <- merge(nSignifGenes_dt, nSignifTADs_dt, by=c("hicds", "exprds"), all=TRUE)
+stopifnot(!is.na(nSignif_dt))
+
+nSignif_dt <- nSignif_dt[order(nSignif_dt$nSignifTADs, decreasing = TRUE),]
+nSignif_dt$dataset <- paste0(nSignif_dt$hicds, "\n", nSignif_dt$exprds)
+
+labcols <- all_cols[all_cmps[nSignif_dt$exprds]]
+
+maxTADs <- max(ceiling(nSignif_dt$nSignifTADs/10)*10)
+maxGenes <- max(ceiling(nSignif_dt$nSignifGenes/1000)*1000)
+
+outFile <- file.path(outFolder, paste0("nSignifGenes_nSignifTADs_all_ds_withSymb.", plotType))
+do.call(plotType, list(outFile, height=myHeight, width=myWidth*2))
+par(bty="U")
+par(mar = c(5,5,2,5))
+plot(
+  x = 1:nrow(nSignif_dt),
+  y = nSignif_dt$nSignifTADs, 
+  col = tad_col,
+  ylim=c(0, maxTADs),
+  ylab = "# signif. TADs",
+  xlab = "",
+  pch=3,
+  main = paste0("# signif. features"),
+  cex.axis=plotCex,
+  cex.main = plotCex,
+  cex.lab = plotCex,
+  col.lab = tad_col,
+  axes = FALSE
+)
+# mtext(side=1, col = labcols, text = nSignif_dt$dataset, at= 1:nrow(nSignif_dt), las=2, cex =0.5)
+axis(2, col = tad_col, col.ticks = tad_col, col.axis=tad_col, at=seq(from=0, to = maxTADs, by=10))
+axis(1, labels=F, lwd.ticks = -1)
+mtext(side=1, col = labcols, text = rep("\u25CF", nrow(nSignif_dt)), at= 1:nrow(nSignif_dt), las=2, cex = 1.2)
+par(new = T)
+plot(
+  x = 1:nrow(nSignif_dt),
+  y = nSignif_dt$nSignifGenes,
+  col = gene_col,
+  ylab = NA,
+  ylim=c(0, maxGenes),
+  xlab = NA,
+  pch=3,
+  cex.axis=plotCex,
+  cex.main = plotCex,
+  cex.lab = plotCex,
+  axes = FALSE
+)
+axis(side=4, col = gene_col, col.ticks = gene_col, col.axis=gene_col, at = seq(from=0, to=maxGenes, by=1000))
+mtext(side = 4, line = 3, '# signif. genes', col=gene_col,  cex=plotCex)
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+
+
+
+
+outFile <- file.path(outFolder, paste0("nSignifGenes_nSignifTADs_all_ds_withLeg.", plotType))
+do.call(plotType, list(outFile, height=myHeight, width=myWidth*2.5))
+par(bty="U")
+par(mar = c(5+2,5,2,5))
+plot(
+  x = 1:nrow(nSignif_dt),
+  y = nSignif_dt$nSignifTADs, 
+  col = tad_col,
+  ylim=c(0, maxTADs),
+  ylab = "# signif. TADs",
+  xlab = "",
+  pch=3,
+  main = paste0("# signif. features"),
+  cex.axis=plotCex,
+  cex.main = plotCex,
+  cex.lab = plotCex,
+  col.lab = tad_col,
+  axes = FALSE
+)
+# mtext(side=1, col = labcols, text = nSignif_dt$dataset, at= 1:nrow(nSignif_dt), las=2, cex =0.5)
+axis(2, col = tad_col, col.ticks = tad_col, col.axis=tad_col, at=seq(from=0, to = maxTADs, by=10))
+axis(1, labels=F, lwd.ticks = -1)
+mtext(side=1, col = labcols, text = nSignif_dt$dataset, at= 1:nrow(nSignif_dt), las=2, cex = 0.6)
+par(new = T)
+plot(
+  x = 1:nrow(nSignif_dt),
+  y = nSignif_dt$nSignifGenes,
+  col = gene_col,
+  ylab = NA,
+  ylim=c(0, maxGenes),
+  xlab = NA,
+  pch=3,
+  cex.axis=plotCex,
+  cex.main = plotCex,
+  cex.lab = plotCex,
+  axes = FALSE
+)
+axis(side=4, col = gene_col, col.ticks = gene_col, col.axis=gene_col, at = seq(from=0, to=maxGenes, by=1000))
+mtext(side = 4, line = 3, '# signif. genes', col=gene_col, cex=plotCex)
+foo <- dev.off()
+cat(paste0("... written: ", outFile, "\n"))
+
+
+
+
+
+
