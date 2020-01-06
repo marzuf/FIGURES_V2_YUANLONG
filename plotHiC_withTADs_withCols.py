15afefc559d6efb31a4da12288b723b62d30dae8
mzuer <z_marie_7@hotmail.com>
2020-01-06 15:05:33 +0100

    060119

--- plotHiC_withTADs_withCols.py
+++ plotHiC_withTADs_withCols.py
@@ -1,6 +1,9 @@
 
 # python plotHiC_withTADs_withCols.py
 
+import os
+import sys
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -37,68 +40,124 @@ def to_matrix(data, chrom1, chrom2,
         m = triu(m, k=1).T + m
     return m
 
-
-
-#def load_tad_list(tad_file, with_labs, **args):
-#    tad_dt = pd.read_csv(tad_file, **args)
-#    
-#    if tad_dt.shape[1] - int(with_labs) == 3:
-#        #return([np.array(tad_dt.iloc[:,1]), np.array(tad_dt.iloc[:,2])])
-#        tad_pos = [list(tad_dt.iloc[:,1]), list(tad_dt.iloc[:,2])]
-#    elif tad_dt.shape[1] - int(with_labs) == 2:
-#        #return([np.array(tad_dt.iloc[:,0]), np.array(tad_dt.iloc[:,1])])
-#        tad_pos = [list(tad_dt.iloc[:,0]), list(tad_dt.iloc[:,1])]
-#    else:
-#            return None
-#        
-#    if with_labs:
-#        tad_pos.append(list(tad_dt.iloc[:,tad_dt.shape[1]-1]))
-#    return tad_pos
-
-# all expected to have chromo-start-end-label-color
-
-def load_tad_list(tad_file, with_labs, **args):
-    tad_dt = pd.read_csv(tad_file, **args)
-    return [list(tad_dt.iloc[:,1]), list(tad_dt.iloc[:,2]), list(tad_dt.iloc[:,3]), list(tad_dt.iloc[:,4])]
-
-
-
-
-# SETTINGS FOR HI-C MATRIX
-#matrix_file = "/media/electron//mnt/ptemp/luca/hic_enhance/data/hic/Rao2014-GM12878-full/Rao2014_observed_NONE_1_1_50000.txt"
-matrix_file = "mat_chr11_40kb_ob.txt"
-resolution = 40000
-
-# SETTINGS FOR TAD DATA
-tad_file = "ENCSR489OCU_NCI-H460_40kb_coord11.txt"  # can be 2-col or 3-col
-plot_labs = True
-
-# SET WHAT TO PLOT:
-chrom = 'chr11'
-map_start=102200001
-map_end=103320000
-
-
-# OUTFILE 
-output_file = "./somewhere.pdf"
+expr_ds = None
+# python plotHiC_withTADs_withCols.py ENCSR489OCU_NCI-H460_40kb chr11_TAD390 mega_ENCSR489OCU_NCI-H460_mat_chr11_40kb_ob.txt
+myargs = sys.argv
+if(len(myargs) == 5):
+    hic_ds = myargs[1]
+    expr_ds = myargs[2]
+    select_tad = myargs[3]
+    matrix_file = myargs[4]
+elif(len(myargs) == 4):
+    hic_ds = myargs[1]
+    select_tad = myargs[2]
+    matrix_file = myargs[3]
+else:    
+    hic_ds = "ENCSR489OCU_NCI-H460_40kb"
+    expr_ds = "TCGAlusc_norm_lusc"
+    matrix_file = "mega_ENCSR489OCU_NCI-H460_mat_chr10_40kb_ob.txt"
+    select_tad = "chr10_TAD268"
+    #matrix_file = "mega_ENCSR489OCU_NCI-H460_mat_chr11_40kb_ob.txt"
+    #select_tad = "chr11_TAD390"
 
 
 # SOME PARAMETERS FOR PLOTTING
+other_col_tad = "black"
+select_col_tad = "green"
+#other_col_lab = "black"
+#select_col_lab = "green"
+nAround_toplot = 0
+nSurroundBins = 2
 shrink = 1
-lwd = 5
+tad_lwd = 1.2
+lab_offset = -0.8
+labSize = 10    
+labBox = True
+addGenes = True
+genesOffset = 0.5
+genesSpacing = 0.5
+symbolOffset = 0.1
+gene_col = "blue"
+gene_lwd = 1
+plot_labs = True
+plotTit = hic_ds
+if expr_ds:
+    plotTit += " - " + expr_ds
 
+if addGenes:
+    assert expr_ds
+
+# PARAMETERS TO SET 
+setDir = "/media/electron"
+setDir = ""
+resolution = 40000
+tad_file  = setDir + "/mnt/etemp/marie/v2_Yuanlong_Cancer_HiC_data_TAD_DA/" + hic_ds + "/genes2tad/all_assigned_regions.txt"
+assert os.path.exists(tad_file)
+
+if addGenes:
+    gene2tad_file  = setDir + "/mnt/etemp/marie/v2_Yuanlong_Cancer_HiC_data_TAD_DA/" + hic_ds + "/genes2tad/all_genes_positions.txt"
+    assert os.path.exists(gene2tad_file)
+    entrezDT_file = setDir + "/mnt/ed4/marie/entrez2synonym/entrez/ENTREZ_POS/gff_entrez_position_GRCh37p13_nodup.txt"
+    assert os.path.exists(entrezDT_file)
+    pipGene_file = setDir + "/mnt/etemp/marie/v2_Yuanlong_Cancer_HiC_data_TAD_DA/PIPELINE/OUTPUT_FOLDER/" + hic_ds + "/" + expr_ds + "/0_prepGeneData" + "/pipeline_geneList.txt"
+    assert os.path.exists(pipGene_file)
+
+out_dir = "PLOTHIC_WITHTADS_WITHCOLS"
+output_file =  out_dir + "/" + hic_ds + "_" + select_tad + ".pdf"
+
+
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+if addGenes:
+    gene2tad_dt = pd.read_csv(gene2tad_file, sep="\t", header=None, names = ['entrezID', 'chromo', 'start', 'end', 'region'] )
+    tad_genes_dt = gene2tad_dt[gene2tad_dt['region'] == select_tad]
+    gff_dt = pd.read_csv(entrezDT_file, sep="\t")
+    gff_dt = gff_dt[['entrezID', 'symbol']]
+    tad_genes_symbols_dt = tad_genes_dt.merge(gff_dt, on='entrezID', how='left')        
+    pipGenes_dt = pd.read_csv(pipGene_file, sep="\t")
+    tad_genes_symbols_dt = tad_genes_symbols_dt[tad_genes_symbols_dt['entrezID'].isin(list(pipGenes_dt['value']))].reset_index(drop=True)
+    assert tad_genes_symbols_dt.shape[0] > 0
+    
+    
+tad_dt = pd.read_csv(tad_file, sep="\t", header=None, names = ['chromo', 'region', 'start', 'end'] )
+
+select_idxs=  tad_dt[tad_dt['region'] == select_tad].index
+assert len(select_idxs) == 1
+select_idx =  int(tad_dt[tad_dt['region'] == select_tad].index[0]) 
+
+chrom = tad_dt['chromo'][select_idx]
+assert re.match(chrom, select_tad)
+
+#start_pos_list = list(tad_dt['start'][(select_idx-nAround_toplot):(select_idx+nAround_toplot+1)])
+start_pos_list = list(tad_dt['start'][(select_idx-nAround_toplot):(select_idx+nAround_toplot+1)] - 1)
+end_pos_list = list(tad_dt['end'][(select_idx-nAround_toplot):(select_idx+nAround_toplot+1)])
+#end_pos_list = list(tad_dt['end'][(select_idx-nAround_toplot):(select_idx+nAround_toplot+1)] + 1)
+lab_list = ([""] * nAround_toplot) + [select_tad] + ([""] * nAround_toplot)
+col_list_tad = ([other_col_tad] * nAround_toplot) + [select_col_tad] + ([other_col_tad] * nAround_toplot)
+#col_list_lab = ([other_col_lab] * nAround_toplot) + [select_col_lab] + ([other_col_lab] * nAround_toplot)
+
+
+
+map_start = start_pos_list[0] - nSurroundBins*resolution
+map_end = end_pos_list[len(end_pos_list)-1] + nSurroundBins*resolution
+
+assert len(start_pos_list) == len(end_pos_list) == len(lab_list) == len(col_list_tad)
+
+tad_toplot = [
+    start_pos_list,
+    end_pos_list,
+    lab_list,
+    col_list_tad
+]
+    
 # LOAD AND SUBSET Hi-C DATA
 data = pd.read_csv(matrix_file, sep="\t", header=None, names=['bin1', 'bin2', 'value'])
 data = data[(data.bin1>=map_start) & (data.bin2<=map_end)]
 m = to_matrix(data, chrom, chrom, resolution, start1=map_start, end1=map_end, start2=map_start, end2=map_end)
 
-# LOAD THE LIST OF TADS TO PLOT
-tad_toplot = load_tad_list(tad_file, with_labs=True, sep="\t", header=None)
-
-
 # retrieve the height of the plot (half-diagonal)
 diagonal_height = np.sqrt(m.shape[0]**2 / 2)
-
 
 my_map = plt.get_cmap("Reds")
 my_map.set_under('white')
@@ -107,7 +166,12 @@ fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 X = np.log10(np.triu(m.todense()*shrink) + 1)
 ax.matshow( rotate(X, 45), cmap=my_map, vmin=0.01 )
 
-plt.ylim(diagonal_height)
+if addGenes:
+    plt.ylim(diagonal_height )
+    #plt.ylim(diagonal_height + genesOffset + genesSpacing*tad_genes_symbols_dt.shape[0])
+else:
+    plt.ylim(diagonal_height)
+
 plt.xticks([])
 plt.yticks([])
 plt.axis('on')
@@ -130,33 +194,81 @@ for tad_start, tad_end , tad_lab, tad_col in zip(tad_toplot[0], tad_toplot[1], t
         [conv_end, diagonal_height]
     ])
 
-    tadTri = plt.Polygon(tad_triangle, facecolor='none', edgecolor=tad_col, linewidth=lwd)
+    tadTri = plt.Polygon(tad_triangle, facecolor='none', edgecolor=tad_col, linewidth=tad_lwd)
     ax.add_patch(tadTri)
 
-    plt.text(0.5*(conv_start+conv_end),tad_height,tad_lab, fontsize=10, horizontalalignment='center', verticalalignment='bottom')
+    if labBox:
+        plt.text(0.5*(conv_start+conv_end),tad_height + lab_offset,tad_lab, fontsize=labSize, horizontalalignment='center', verticalalignment='bottom',
+             bbox=dict(facecolor=tad_col, alpha=0.5), fontweight='bold')
+    else:
+        plt.text(0.5*(conv_start+conv_end),tad_height + lab_offset,tad_lab, fontsize=labSize, horizontalalignment='center', verticalalignment='bottom', fontweight='bold')
+    
+    
+if addGenes:
+    from matplotlib.patches import Rectangle
+    from matplotlib import collections  as mc
+    #lines = [[(0, 50), (20, 50)], [(50, 50), (70, 50)]]
+    #c = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
+    #c = ['black', 'green']
+    #lc = mc.LineCollection(lines, colors=c, linewidths=2)
+    #fig, ax = pl.subplots()
+    #ax.add_collection(lc)
+    genePos = diagonal_height.item() + genesOffset
+    
+    
+#     first_start = start_pos_list[0]
+#     first_end =  0.5*( start_pos_list[0] + end_pos_list[len(end_pos_list)-1])
+#     first_lab = "FOO_FIRST_GENE"
+#     conv_gene_start = np.sqrt(2) * (first_start - map_start)/resolution
+#     conv_gene_end = np.sqrt(2) * (first_end - map_start)/resolution
+#     print("FIRST");print(conv_gene_start);print(genePos);print(conv_gene_end-conv_gene_start)
+#     ax.add_patch(Rectangle((conv_gene_start, genePos), (conv_gene_end-conv_gene_start), height=0.2,
+#                            facecolor=gene_col, clip_on=False))
+#     plt.text(0.5*(conv_gene_start+conv_gene_end), genePos-symbolOffset, first_lab, fontsize=8, horizontalalignment='center', verticalalignment='center')
+#     genePos += genesSpacing
+        
 
     
-#if plot_labs:
-#    for tad_start, tad_end, tad_lab in zip(tad_toplot[0], tad_toplot[1], tad_toplot[2]):
-#        tad_start_bin = (tad_start - map_start)//resolution  
-#        tad_end_bin = (tad_end - map_start)//resolution
-
-#        conv_start = np.sqrt(2) * tad_start_bin  # distance to bin start: on the side of the square, it is the # of bins but in triangle plot, it is the diagonal
-#        conv_end = np.sqrt(2) * tad_end_bin
-#        
-
-#        tad_height = diagonal_height-(conv_end-conv_start)*0.5  # needs the "diagonal_heigh" because the way it is plotted and the orientation of the y-axis
-#        
-#        plt.text(0.5*(conv_start+conv_end),tad_height,tad_lab, fontsize=10, horizontalalignment='center', verticalalignment='bottom')
-#        #
-#        # plt.text(0.5*(conv_start+conv_end),tad_height,tad_lab, fontsize=10)
     
+    for i in range(tad_genes_symbols_dt.shape[0]):
+        conv_gene_start = np.sqrt(2) * (tad_genes_symbols_dt['start'][i] - map_start)/resolution
+        conv_gene_end = np.sqrt(2) * (tad_genes_symbols_dt['end'][i] - map_start)/resolution
+        #plt.plot([conv_gene_start, genePos], [conv_gene_end, genePos], color=gene_col, lw=gene_lwd)
+        #lines = [[(conv_gene_start, genePos), (conv_gene_end, genePos)]]
+        #c = [gene_col]
+        #lc = mc.LineCollection(lines, colors=c, linewidths=2)
+        #ax.add_collection(lc)
+        #plt.add_collection(lc)
+        #plt.text(conv_gene_start, genePos,tad_genes_symbols_dt['symbol'][i], fontsize=10, horizontalalignment='right', verticalalignment='center', fontweight='bold')
+        print("i="+str(i));print(conv_gene_start);print(genePos);print(conv_gene_end-conv_gene_start)
+        ax.add_patch(Rectangle((conv_gene_start, genePos), (conv_gene_end-conv_gene_start), height=0.2,
+                               facecolor=gene_col, clip_on=False))
+        plt.text(0.5*(conv_gene_start+conv_gene_end), genePos-symbolOffset,tad_genes_symbols_dt['symbol'][i], fontsize=8, horizontalalignment='center', verticalalignment='center')
+
+        genePos += genesSpacing
+        
+        
+        
+        
+#     last_start = 0.5*( start_pos_list[0] + end_pos_list[len(end_pos_list)-1])
+#     last_end =  end_pos_list[len(end_pos_list)-1]
+#     last_lab = "FOO_LAST_GENE"
+#     conv_gene_start = np.sqrt(2) * (last_start - map_start)/resolution
+#     conv_gene_end = np.sqrt(2) * (last_end - map_start)/resolution
+#     print("LAST");print(conv_gene_start);print(genePos);print(conv_gene_end-conv_gene_start)
+#     ax.add_patch(Rectangle((conv_gene_start, genePos), (conv_gene_end-conv_gene_start), height=0.2,
+#                            facecolor=gene_col, clip_on=False))
+#     plt.text(0.5*(conv_gene_start+conv_gene_end), genePos-symbolOffset, last_lab, fontsize=8, horizontalalignment='center', verticalalignment='center')
+#     genePos += genesSpacing
+    
+        
+        
+
+   
+plt.title(plotTit, fontweight="bold")    
 
 if output_file:
-	plt.savefig(output_file)
+	plt.savefig(output_file, bbox_inches='tight',transparent=True)
+    
 	print("... saved: " + output_file + "\n")
     
-
-    
-
-
---
+++ plotHiC_withTADs_withCols_v0.py
@@ -0,0 +1,206 @@
+
+# python plotHiC_withTADs_withCols.py
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage import rotate
+from scipy.sparse import coo_matrix, triu
+
+def to_matrix(data, chrom1, chrom2, 
+              resolution, assembly="hg19",
+              start1=None, end1=None, 
+              start2=None, end2=None):
+    
+    if all([x is not None for x in [start1, end1, start2, end2]]):
+        start_bin1 = start1//resolution
+        end_bin1 = end1//resolution
+        start_bin2 = start2//resolution
+        end_bin2 = end2//resolution
+    else:
+        chromsize = load_chromsizes(assembly)
+        chromsize1 = chromsize[chrom1]
+        chromsize2 = chromsize[chrom2]
+        start_bin1 = 0
+        end_bin1 = chromsize1//resolution
+        start_bin2 = 0
+        end_bin2 = chromsize2//resolution
+    
+    n_bins_1 = end_bin1 - start_bin1 + 1#int(chromsize1 / resolution) + 1
+    n_bins_2 = end_bin2 - start_bin2 + 1#int(chromsize2 / resolution) + 1
+    
+    bin1s = data.bin1//resolution - start_bin1#(data.bin1 / resolution).astype(int).values
+    bin2s = data.bin2//resolution - start_bin2#(data.bin2 / resolution).astype(int).values
+    values = data.value.values
+    m = coo_matrix( (values, (bin1s, bin2s)), shape=(n_bins_1, n_bins_2) )
+    if chrom1 == chrom2:
+        m = triu(m, k=1).T + m
+    return m
+
+
+
+#def load_tad_list(tad_file, with_labs, **args):
+#    tad_dt = pd.read_csv(tad_file, **args)
+#    
+#    if tad_dt.shape[1] - int(with_labs) == 3:
+#        #return([np.array(tad_dt.iloc[:,1]), np.array(tad_dt.iloc[:,2])])
+#        tad_pos = [list(tad_dt.iloc[:,1]), list(tad_dt.iloc[:,2])]
+#    elif tad_dt.shape[1] - int(with_labs) == 2:
+#        #return([np.array(tad_dt.iloc[:,0]), np.array(tad_dt.iloc[:,1])])
+#        tad_pos = [list(tad_dt.iloc[:,0]), list(tad_dt.iloc[:,1])]
+#    else:
+#            return None
+#        
+#    if with_labs:
+#        tad_pos.append(list(tad_dt.iloc[:,tad_dt.shape[1]-1]))
+#    return tad_pos
+
+# all expected to have chromo-start-end-label-color
+
+def load_tad_list(tad_file, with_labs, **args):
+    tad_dt = pd.read_csv(tad_file, **args)
+    return [list(tad_dt.iloc[:,1]), list(tad_dt.iloc[:,2]), list(tad_dt.iloc[:,3]), list(tad_dt.iloc[:,4])]
+
+
+# SETTINGS FOR HI-C MATRIX
+#matrix_file = "/media/electron//mnt/ptemp/luca/hic_enhance/data/hic/Rao2014-GM12878-full/Rao2014_observed_NONE_1_1_50000.txt"
+#matrix_file = "mat_chr11_40kb_ob.txt"
+matrix_file = "mega_ENCSR489OCU_NCI-H460_mat_chr10_40kb_ob.txt"
+resolution = 40000
+
+# SETTINGS FOR TAD DATA
+#tad_file = "ENCSR489OCU_NCI-H460_40kb_coord_chr11.txt"  # can be 2-col or 3-col chr11_TAD390
+tad_file = "ENCSR489OCU_NCI-H460_40kb_coord_chr10.txt"  # can be 2-col or 3-col chr10_TAD268
+plot_labs = True
+
+# SET WHAT TO PLOT:
+chrom = 'chr11'
+map_start=102200001
+map_end=103320000
+
+chrom='chr10'
+map_start = 80120001
+map_end = 80120001
+
+
+# OUTFILE 
+output_file = "PLOTHIC_WITHTADS_WITHCOLS/ENCSR489OCU_NCI-H460_40kb_chr11_TAD390.pdf"
+output_file = "PLOTHIC_WITHTADS_WITHCOLS/ENCSR489OCU_NCI-H460_40kb_chr10_TAD268.pdf"
+
+
+setDir = "/media/electron"
+hic_ds = "ENCSR489OCU_NCI-H460_40kb"
+select_tad = "chr11_TAD390"
+
+other_col = "black"
+select_col = "green"
+
+nAround_toplot = 2
+
+tad_file  = setDir + "/mnt/etemp/marie/v2_Yuanlong_Cancer_HiC_data_TAD_DA/" + hic_ds + "/genes2tad/all_assigned_regions.txt"
+assert os.path.exists(tad_file)
+
+
+tad_dt = pd.read_csv(tad_file, sep="\t", header=None, names = ['chromo', 'region', 'start', 'end'] )
+
+
+select_idxs=  tad_dt[tad_dt['region']==select_tad].index
+assert len(select_idxs) == 1
+select_idx =  int(tad_dt[tad_dt['region']==select_tad].index[0]) 
+
+start_pos_list = list(tad_dt['start'][(select_idx-nAround_toplot):(select_idx+nAround_toplot+1)])
+end_pos_list = list(tad_dt['end'][(select_idx-nAround_toplot):(select_idx+nAround_toplot+1)])
+lab_list = ([""] * nAround_toplot) + [select_tad] + ([""] * nAround_toplot)
+col_list = ([other_col] * nAround_toplot) + [select_col] + ([other_col] * nAround_toplot)
+
+assert len(start_pos_list) == len(end_pos_list) == len(lab_list) == len(col_list)
+
+tad_toplot = [
+    start_pos_list,
+    end_pos_list,
+    lab_list,
+    col_list
+]
+
+chrom = tad_dt['chromo'][select_idx]
+
+
+
+# SOME PARAMETERS FOR PLOTTING
+shrink = 1
+lwd = 5
+
+# LOAD AND SUBSET Hi-C DATA
+data = pd.read_csv(matrix_file, sep="\t", header=None, names=['bin1', 'bin2', 'value'])
+data = data[(data.bin1>=map_start) & (data.bin2<=map_end)]
+m = to_matrix(data, chrom, chrom, resolution, start1=map_start, end1=map_end, start2=map_start, end2=map_end)
+
+# LOAD THE LIST OF TADS TO PLOT
+# tad_toplot = load_tad_list(tad_file, with_labs=True, sep="\t", header=None)
+
+
+# retrieve the height of the plot (half-diagonal)
+diagonal_height = np.sqrt(m.shape[0]**2 / 2)
+
+
+my_map = plt.get_cmap("Reds")
+my_map.set_under('white')
+#fig, ax = plt.subplots(1, 1, figsize=(40, 40))
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+X = np.log10(np.triu(m.todense()*shrink) + 1)
+ax.matshow( rotate(X, 45), cmap=my_map, vmin=0.01 )
+
+plt.ylim(diagonal_height)
+plt.xticks([])
+plt.yticks([])
+plt.axis('on')
+
+
+for tad_start, tad_end , tad_lab, tad_col in zip(tad_toplot[0], tad_toplot[1], tad_toplot[2], tad_toplot[3]):
+
+    tad_start_bin = (tad_start - map_start)//resolution  
+    tad_end_bin = (tad_end - map_start)//resolution
+
+
+    conv_start = np.sqrt(2) * tad_start_bin  # distance to bin start: on the side of the square, it is the # of bins but in triangle plot, it is the diagonal
+    conv_end = np.sqrt(2) * tad_end_bin
+
+    tad_height = diagonal_height-(conv_end-conv_start)*0.5  # needs the "diagonal_heigh" because the way it is plotted and the orientation of the y-axis
+    
+    tad_triangle = np.array([
+        [conv_start,diagonal_height],
+        [0.5*(conv_start+conv_end), tad_height],
+        [conv_end, diagonal_height]
+    ])
+
+    tadTri = plt.Polygon(tad_triangle, facecolor='none', edgecolor=tad_col, linewidth=lwd)
+    ax.add_patch(tadTri)
+
+    plt.text(0.5*(conv_start+conv_end),tad_height,tad_lab, fontsize=10, horizontalalignment='center', verticalalignment='bottom')
+
+    
+#if plot_labs:
+#    for tad_start, tad_end, tad_lab in zip(tad_toplot[0], tad_toplot[1], tad_toplot[2]):
+#        tad_start_bin = (tad_start - map_start)//resolution  
+#        tad_end_bin = (tad_end - map_start)//resolution
+
+#        conv_start = np.sqrt(2) * tad_start_bin  # distance to bin start: on the side of the square, it is the # of bins but in triangle plot, it is the diagonal
+#        conv_end = np.sqrt(2) * tad_end_bin
+#        
+
+#        tad_height = diagonal_height-(conv_end-conv_start)*0.5  # needs the "diagonal_heigh" because the way it is plotted and the orientation of the y-axis
+#        
+#        plt.text(0.5*(conv_start+conv_end),tad_height,tad_lab, fontsize=10, horizontalalignment='center', verticalalignment='bottom')
+#        #
+#        # plt.text(0.5*(conv_start+conv_end),tad_height,tad_lab, fontsize=10)
+    
+
+if output_file:
+	plt.savefig(output_file)
+	print("... saved: " + output_file + "\n")
+    
+
+    
+
+
